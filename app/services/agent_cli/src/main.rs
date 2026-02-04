e71d69c9045d953ee5cd4cfb1dd884f21d912bad
Tony Yousefnezhad <myousefnezhad@gmail.com>
2026-02-04 16:05:38 -0700

    Agent cli

--- app/services/agent_cli/src/main.rs
+++ app/services/agent_cli/src/main.rs
@@ -3,8 +3,8 @@ use adk_model::{OpenAIClient, OpenAIConfig};
 use adk_rust::Launcher;
 use adk_rust::prelude::*;
 use app_config::AppConfig;
-use rmcp::ServiceExt;
-use rmcp::transport::streamable_http_client::{
+use rmcp09::ServiceExt;
+use rmcp09::transport::streamable_http_client::{
     StreamableHttpClientTransport, StreamableHttpClientTransportConfig,
 };
 use std::sync::Arc;
@@ -17,10 +17,8 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
         .auth_header(&config.mcp_token.clone());
     let transport = StreamableHttpClientTransport::from_config(mcp_cfg);
     let mcp_client = ().serve(transport).await?;
-
-    //let mcp_client = ().serve(transport).await?;
     let toolset = McpToolset::new(mcp_client);
-    let cancel = toolset.cancellation_token().await;
+    let _cancel = toolset.cancellation_token().await;
     let ctx: Arc<dyn ReadonlyContext> = Arc::new(SimpleContext);
     let tools = toolset.tools(ctx).await?;
 
@@ -38,9 +36,14 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
         .description("This is my first AI Agent")
         .instruction("You are a very frindly agent answering the questions")
         .model(Arc::new(model));
+    // Add MCP tools
     for t in tools {
         builder = builder.tool(t);
     }
+    // Add Google Search Tools
+    // Note: compatible with Gemini 2 models
+    // Need export GOOGLE_API_KEY="YOUR_GOOGLE_KEY"
+    builder = builder.tool(Arc::new(GoogleSearchTool::new()));
     let agent = Arc::new(builder.build()?);
 
     Launcher::new(agent).run().await?;
